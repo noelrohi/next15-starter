5ea6739affd3f3bbda4b2c7e980f7271ab8a5575
gneiru <noelrohi59@gmail.com>
2024-12-25 22:00:15 +0800

    Enhance post retrieval to include author information and order by creation date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import { db } from "@/db";
 import { CreatePost } from "./_components/create-post";
 
 export default async function Page() {
-  const posts = await db.query.post.findMany();
+  const posts = await db.query.post.findMany({
+    with: {
+      author: true,
+    },
+    orderBy: (post, { desc }) => [desc(post.createdAt)],
+  });
   return (
     <div className="container mx-auto flex flex-col gap-4 py-10">
       <CreatePost />
@@ -12,6 +17,9 @@ export default async function Page() {
           <p className="mt-2 text-gray-600">{post.content}</p>
           <div className="mt-4 text-gray-500 text-sm">
             Posted on {new Date(post.createdAt ?? "").toLocaleDateString()}
+          </div>
+          <div className="mt-4 text-gray-500 text-sm">
+            Author: {post.author?.name}
           </div>
         </div>
       ))}
